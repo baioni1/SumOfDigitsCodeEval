c3192de2d1b89a0a29a60b5d4ecae5fb001fc517
Your Name <your@example.com>
2016-05-14 17:38:57 -0400

    SumOfDigits_Initial

---
+++ sum of digits code eval/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_digits_code_eval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "496";// assign a string variable already assigned in code evil
+            char[] digits = line.ToCharArray();
+            int sumTotal = 0;
+            foreach (char digit in digits)
+            {
+                sumTotal += int.Parse(char.ToString(digit));
+            }
+
+            Console.WriteLine(sumTotal); 
+        }
+    }
+}
